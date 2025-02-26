6eff0f6311d3d22c47d407a57bb286f254e81e7a
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2025-02-26 16:50:13 +0300

    refactor: update TrackOrder function to use environment variable for BaseURL and comment out apiCall

--- actions/order.ts
+++ actions/order.ts
@@ -1,13 +1,14 @@
-import apiCall from '@/base-api/api';
+// import apiCall from '@/base-api/api';
 import { orderTrack } from '@/types/orderTrack';
 import { endPoints } from '@/data/endPoints';
 
-const BaseURL = 'https://api.example.com';
+const BaseURL = process.env.BASE_API;
 const url = `${BaseURL}/${endPoints.anOrder}`;
 export const TrackOrder = async ({ id }: orderTrack) => {
   console.log('id:', id);
   console.log('url:', url);
   const fetchURl = `${url}?${id}`;
-  const response = await apiCall({ url: fetchURl });
-  return response;
+  console.log('fetchURl:', fetchURl);
+  //   const response = await apiCall({ url: fetchURl });
+  //   return response;
 };
--- data/endPoints.ts
+++ data/endPoints.ts
@@ -1,3 +1,3 @@
 export const endPoints = {
-  anOrder: 'order',
+  anOrder: 'orders',
 };
