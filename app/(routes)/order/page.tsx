29e7475731964bbe49ace9bb813a3c3ae576be1f
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2025-02-26 12:01:33 +0300

    feat: update padding for hero section on order tracking page for improved layout

--- app/(routes)/order/page.tsx
+++ app/(routes)/order/page.tsx
@@ -33,7 +33,7 @@ export default function Page() {
       </nav>
 
       {/* Hero Section */}
-      <section className="w-full h-[320px] md:h-[720px] px-6 md:px-[120px] py-[183px] bg-[linear-gradient(to_right,_rgba(0,0,0,0.7),rgba(0,0,0,0)),url('/images/track-order-banner.jpg')] flex-col justify-center items-start gap-4 inline-flex overflow-hidden bg-cover bg-center bg-no-repeat md:bg-cover">
+      <section className="w-full h-[320px] md:h-[720px] px-6  lg:px-[120px] py-[183px] bg-[linear-gradient(to_right,_rgba(0,0,0,0.7),rgba(0,0,0,0)),url('/images/track-order-banner.jpg')] flex-col justify-center items-start gap-4 inline-flex overflow-hidden bg-cover bg-center bg-no-repeat md:bg-cover">
         <div className="w-64 md:w-[646px] text-white text-2xl md:text-[64px] font-extrabold font-['Manrope'] md:leading-tight">
           Fast. Reliable. Delivered with Care.
         </div>
