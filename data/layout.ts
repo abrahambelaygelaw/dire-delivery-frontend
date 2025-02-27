17f7965f6d47a6da6724dba0af62788f3f7a6d2d
Nathnael Desta <destanathnaelataro@gmail.com>
2025-02-27 07:03:22 +0300

    fix: fix some eslint issues

--- data/layout.ts
+++ data/layout.ts
@@ -1,30 +1,30 @@
-import { ClipboardList, Settings, UserCog, Users } from "lucide-react"
+import { ClipboardList, Settings, UserCog, Users } from 'lucide-react';
 
 // Menu items.
 export const menuItems = [
-    {
-      title: "Employees",
-      url: "/admin/employees",
-      icon: Users,
-    },
-    {
-      title: "Admins",
-      url: "/admin/admins",
-      icon: UserCog,
-    },
-    {
-      title: "Orders",
-      url: "/admin/orders",
-      icon: ClipboardList,
-    },
-    {
-      title: "Settings",
-      url: "/admin/settings",
-      icon: Settings,
-    },
-    {
-      title: "Dashboard",
-      url: "/admin",
-      icon: "",
-    }
-  ]
+  {
+    title: 'Employees',
+    url: '/admin/employees',
+    icon: Users,
+  },
+  {
+    title: 'Admins',
+    url: '/admin/admins',
+    icon: UserCog,
+  },
+  {
+    title: 'Orders',
+    url: '/admin/orders',
+    icon: ClipboardList,
+  },
+  {
+    title: 'Settings',
+    url: '/admin/settings',
+    icon: Settings,
+  },
+  {
+    title: 'Dashboard',
+    url: '/admin',
+    icon: '',
+  },
+];
