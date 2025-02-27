125a5cb80e2e21e6ed6598d26b0d1a03f692c729
Nathnael Desta <destanathnaelataro@gmail.com>
2025-02-27 06:19:22 +0300

    fix: fix selected tab not changing when clicking dashboard

--- components/shared/layout/app-sidebar.tsx
+++ components/shared/layout/app-sidebar.tsx
@@ -43,6 +43,11 @@ const items = [
     url: "/admin/settings",
     icon: Settings,
   },
+  {
+    title: "Dashboard",
+    url: "/admin",
+    icon: "",
+  }
 ]
 
 export function AppSidebar() {
@@ -50,38 +55,38 @@ export function AppSidebar() {
   const [selectedItem, setSelectedItem] = useState("");
 
   useEffect(() => {
-    const matchedItem = items.find((item) => pathname.startsWith(item.url));
+    const matchedItem = items.find((item) => pathname == item.url);
     setSelectedItem(matchedItem ? matchedItem.title : "");
     console.log("item", matchedItem)
   }, [pathname]); // Re-run when pathname changes
 
   return (
     <Sidebar>
-    <SidebarContent className="bg-[#060A87] text-white py-6 px-3 flex flex-col gap-6">
-      <div className="flex justify-between items-center">
-        <Image src={direLogo} alt="dire logo" width={185} />
-      </div>
-      <Link href="/admin" className="w-full py-4 px-5 flex justify-between items-center rounded-[10px] font-bold bg-[#C7E7F6F5]">
-        <div className="text-[#060A87] flex items-center justify-center">Dashboard</div>
-        <LuLayoutGrid stroke="#060A87" size={24} />
-      </Link>
-      <SidebarGroup className="p-0">
-        <SidebarGroupContent>
-          <SidebarMenu>
-            {items.map((item) => (
-              <SidebarMenuItem key={item.title}>
-                <SidebarMenuButton asChild>
-                  <a href={item.url} className={cn("h-[50px] flex gap-2.5 p-0", selectedItem == item.title && "bg-[#030661] px-3")}>
-                    <item.icon />
-                    <span className="text-sm font-bold">{item.title}</span>
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            ))}
-          </SidebarMenu>
-        </SidebarGroupContent>
-      </SidebarGroup>
-    </SidebarContent>
-  </Sidebar>
+      <SidebarContent className="bg-[#060A87] text-white py-6 px-3 flex flex-col gap-6">
+        <div className="flex justify-between items-center">
+          <Image src={direLogo} alt="dire logo" width={185} />
+        </div>
+        <a href="/admin" className="w-full py-4 px-5 flex justify-between items-center rounded-[10px] font-bold bg-[#C7E7F6F5]">
+          <div className="text-[#060A87] flex items-center justify-center">Dashboard</div>
+          <LuLayoutGrid stroke="#060A87" size={24} />
+        </a>
+        <SidebarGroup className="p-0">
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                item.title !== "Dashboard" && <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url} className={cn("h-[50px] flex gap-2.5 p-0", selectedItem == item.title && "bg-[#030661] px-3")}>
+                      <item.icon />
+                      <span className="text-sm font-bold">{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
   )
 }
