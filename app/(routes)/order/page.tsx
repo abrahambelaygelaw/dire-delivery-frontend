f8af75c7d9adddccd6b102e2638a566a293ba20b
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2025-02-28 22:14:47 +0300

    feat: Enhance order not found message with improved UI and image

--- app/(routes)/order/page.tsx
+++ app/(routes)/order/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import Confused from '@/public/images/confused.jpg';
 import plane from '@/public/Icons/plane.svg';
 import { LuMapPin } from 'react-icons/lu';
 import {
@@ -352,7 +353,18 @@ export default function Page() {
               ;
             </section>
           ) : (
-            <h1>No item found</h1>
+            <section className='className="flex flex-col justify-center items-center gap-2 md:gap-8 border-t-2 border-[#060a87] py-6 md:py-12 px-2 lg:px-3  w-full '>
+              <h1 className="w-full text-center font-bold text-xl md:text-2xl lg:text-3xl">
+                Order was Not Found!
+              </h1>
+              <div className="w-full flex justify-center mt-4">
+                <Image
+                  src={Confused}
+                  alt="Order not Found"
+                  className="w-[280px] md:w-[360px] lg:w-[400px]"
+                />
+              </div>
+            </section>
           )
         ) : null}
 
