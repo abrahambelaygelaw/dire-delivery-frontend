91207d7ca5a6add5fe5e53162841b69b3c4986a9
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2025-03-13 22:49:32 +0300

    feat: update FTP secrets in deployment workflow and add admin settings page component

---
+++ app/(routes)/admin/setting/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminSettingPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Admin Settings</h1>
+            <p>This is the admin settings page. More content coming soon!</p>
+        </div>
+    );
+};
+
+export default AdminSettingPage;
